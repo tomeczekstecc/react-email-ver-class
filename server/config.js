3660a71c962ea55aad3517cc380372df2cbef89d
Tomasz Steć <tomeczekstecc@gmail.com>
2020-12-26 17:04:24 +0100

    first

---
+++ server/config.js
@@ -0,0 +1,10 @@
+exports.PORT = process.env.PORT || 5000
+
+exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production' 
+  ? process.env.CLIENT_ORIGIN
+  : 'http://localhost:3000'
+
+exports.DB_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_URL
+    : 'mongodb://admin:secret@localhost:27020/ver';
